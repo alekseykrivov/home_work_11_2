c36d0c89776c90193cdbc18ec185b1fd2c7e2c00
Aleksey Krivov <adkrivov@gmail.com>
2023-02-14 11:10:45 +0300

    Add def first_letter_string in str_func.py

--- str_func.py
+++ str_func.py
@@ -1,2 +1,7 @@
 def capital_letters_string(str_lower):
     return str_lower.upper()
+
+def first_letter_string(input_string):
+    '''Return first capitalized
+     letter'''
+    return input_string.title()
